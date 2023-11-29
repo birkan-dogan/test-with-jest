e60019fc4e55490b3ef64bb8806afebdbc0108fb
birkan-dogan <b.561birkan@gmail.com>
2023-11-29 10:44:54 +0300

    test inputs and button 🛸

--- src/app/login/page.test.tsx
+++ src/app/login/page.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Login from './page';
 
 // check elements in DOM
@@ -19,4 +19,71 @@ it('button should be rendered', () => {
   render(<Login />);
   const button = screen.getByRole('button');
   expect(button).toBeInTheDocument();
+});
+
+// control the inputs that they are empty
+it('input should be empty', () => {
+  render(<Login />);
+  const userInputEl = screen.getByPlaceholderText(/username/i) as HTMLInputElement;
+  expect(userInputEl.value).toBe('');
+});
+
+it('input should be empty', () => {
+  render(<Login />);
+  const passwordEl = screen.getByPlaceholderText(/password/i) as HTMLInputElement;
+  expect(passwordEl.value).toBe('');
+});
+
+// button should be disabled to avoid unnecessary requests when inputs are empty
+it('button should be disabled', () => {
+  render(<Login />);
+  const button = screen.getByRole('button');
+  expect(button).toBeDisabled();
+});
+
+// error message should be invisible to the users
+it('error message should be invisible', () => {
+  render(<Login />);
+  const message = screen.getByTestId('error');
+  expect(message).toHaveClass('invisible');
+});
+
+// username input should be changed
+it('username input should be changed', () => {
+  render(<Login />);
+  const userInputEl = screen.getByPlaceholderText(/username/i) as HTMLInputElement;
+
+  const testValue = 'test';
+
+  fireEvent.change(userInputEl, { target: { value: testValue } });
+
+  expect(userInputEl.value).toBe(testValue);
+});
+
+// password input should be changed
+it('password input should be changed', () => {
+  render(<Login />);
+  const passwordEl = screen.getByPlaceholderText(/password/i) as HTMLInputElement;
+
+  const testValue = 'test';
+
+  fireEvent.change(passwordEl, { target: { value: testValue } });
+
+  expect(passwordEl.value).toBe(testValue);
+});
+
+// button should not be disabled when inputs exist
+it('button should not be disabled', () => {
+  render(<Login />);
+  const button = screen.getByRole('button');
+
+  const userInputEl = screen.getByPlaceholderText(/username/i) as HTMLInputElement;
+  const passwordEl = screen.getByPlaceholderText(/password/i) as HTMLInputElement;
+
+  const testValue = 'test';
+
+  fireEvent.change(userInputEl, { target: { value: testValue } });
+  fireEvent.change(passwordEl, { target: { value: testValue } });
+
+  expect(button).not.toBeDisabled();
 });
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,50 @@
+'use client';
+
+import { useState } from 'react';
+
 export default function Login() {
+  const [error, setError] = useState<boolean>(false);
+  const [inputs, setInputs] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInputs((prevInputs) => ({
+      ...prevInputs,
+      [name]: value,
+    }));
+  };
+
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-white">
+    <div className="w-screen h-screen flex justify-center items-center bg-white text-black">
       <form className="flex flex-col">
         <input
           type="text"
           placeholder="username"
+          name="username"
           className="py-2 px-4 mb-4 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+          onChange={handleChange}
+          value={inputs.username}
         />
         <input
           type="password"
+          name="password"
           placeholder="password"
           className="py-2 px-4 mb-4 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+          value={inputs.password}
+          onChange={handleChange}
         />
-        <button className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none">Login</button>
+        <button
+          className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none"
+          disabled={inputs.username && inputs.password ? false : true}
+        >
+          Login
+        </button>
+        <span data-testid="error" className={`text-center mt-5 text-red-500 ${error ? 'visible' : 'invisible'}`}>
+          Something went wrong!
+        </span>
       </form>
     </div>
   );
